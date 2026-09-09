46e2a1862b601aa962aebe974a22c5bcf9a5b161
Marta Kikkas <martakikkas@gmail.com>
2026-09-09 13:49:53 +0300

    ass traits example

--- index.php
+++ index.php
@@ -1,48 +1,23 @@
 <?php 
 
-class Box {
-    public $width;
-    private $height;
-    protected $length;
-    public bool $isOpen = false;
-    public bool $hasBeenOpened = false;
+class Cat {
+    use MakesSound;
+}
 
-    public function open() {
-        $this->isOpen = true;
-        $this->hasBeenOpened = true;
-    }
+class Dog {
+    use HasSmell, MakesSound;
+}
 
-    public function close() {
-        $this->isOpen = false;
-    }
-
-    public function volume() {
-        return $this->height * $this->length * $this->width;
-    }
-
-    public function test(){
-        var_dump($this->length);
-    }
-
-    public function setHeight(int $height) {
-        if($height > 0) {
-            $this->height = $height;
-        }
-    }
-
-    public function getHeight() {
-        return $this->height;
+trait HasSmell {
+    public $smell;
+    public function sniff() {
+        return 'Smells like ' . $this->smell;
     }
 }
 
-class MetalBox extends Box {
-    public $weight;
-
-    public function mass(){
-        return $this->volume() * $this->weight;
+trait MakesSound {
+    public $sound;
+    public function noise() {
+        return 'sounds like' . $this->sound;
     }
 }
-
-$metal1 = new Box();
-var_dump($metal1->height);
-var_dump($metal1);
