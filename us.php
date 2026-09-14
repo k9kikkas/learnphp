376a86c91df701241ed9c483db48b37e030505ec
Marta Kikkas <martakikkas@gmail.com>
2026-09-14 15:19:34 +0300

    add U.S page and posts and title

--- us.php
+++ us.php
@@ -1,27 +1,27 @@
 <?php
-$title = 'World';
+$title = 'U.S';
 $posts = [
   [
-    'title' => 'Some world title 1', 
-    'content' => 'Some world content 1', 
+    'title' => 'Some U.S title 1', 
+    'content' => 'Some U.S content 1', 
     'date' => 'Jan 1, 2026', 
     'author' => 'Pets',
   ],
   [
-    'title' => 'Some world title 2', 
-    'content' => 'Some world content 2', 
+    'title' => 'Some U.S title 2', 
+    'content' => 'Some U.S content 2', 
     'date' => 'Nov 29, 2023', 
     'author' => 'Steve',
   ],
   [
-    'title' => 'Some world title 3', 
-    'content' => 'Some world content 3', 
+    'title' => 'Some U.S title 3', 
+    'content' => 'Some U.S content 3', 
     'date' => 'May 20, 2009', 
     'author' => 'Alex',
   ],
   [
-    'title' => 'Some world title 4', 
-    'content' => 'Some world content 4', 
+    'title' => 'Some U.S title 4', 
+    'content' => 'Some U.S content 4', 
     'date' => 'Sept 13, 2018', 
     'author' => 'Tommi sularaha',
   ],
@@ -33,7 +33,6 @@ $posts = [
 <?php include __DIR__ . '/partials/header.php'; ?>
     
     <main class="container">
-      <?php include __DIR__ . '/partials/hero.php'; ?>
       <?php include __DIR__ . '/partials/featured.php'; ?>
 
       <div class="row g-5">
