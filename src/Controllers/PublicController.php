b0651211d74ec99e06559a90f6a2726064ab47ea
Marta Kikkas <martakikkas@gmail.com>
2026-09-23 10:01:06 +0300

    Show articles from database

--- src/Controllers/PublicController.php
+++ src/Controllers/PublicController.php
@@ -10,75 +10,22 @@ class PublicController
     {
         
         $articles = Article::all();
-        dump($articles);
-        $users = User::all();
-        dump($users);
-        // $title = 'World';
-        // view('index', compact('title', 'posts'));
+        $title = 'World';
+        view('index', compact('title', 'articles'));
     }
 
     public function us()
     {
+        $articles = Article::all();
         $title = 'U.S';
-        $posts = [
-            [
-                'title' => 'Some U.S title 1', 
-                'content' => 'Some U.S content 1', 
-                'date' => 'Jan 1, 2026', 
-                'author' => 'Pets',
-            ],
-            [
-                'title' => 'Some U.S title 2', 
-                'content' => 'Some U.S content 2', 
-                'date' => 'Nov 29, 2023', 
-                'author' => 'Steve',
-            ],
-            [
-                'title' => 'Some U.S title 3', 
-                'content' => 'Some U.S content 3', 
-                'date' => 'May 20, 2009', 
-                'author' => 'Alex',
-            ],
-            [
-                'title' => 'Some U.S title 4', 
-                'content' => 'Some U.S content 4', 
-                'date' => 'Sept 13, 2018', 
-                'author' => 'Tommi sularaha',
-            ],
-        ];
-        view('us', compact('title', 'posts'));
+        view('us', compact('title', 'articles'));
     }
 
     public function tech()
     {
+        $articles = Article::all();
         $title = 'Tech';
-        $posts = [
-            [
-              'title' => 'Some Tech title 1', 
-              'content' => 'Some Tech content 1', 
-              'date' => 'Jan 1, 2026', 
-              'author' => 'Pets',
-            ],
-            [
-              'title' => 'Some Tech title 2', 
-              'content' => 'Some Tech content 2', 
-              'date' => 'Nov 29, 2023', 
-              'author' => 'Steve',
-            ],
-            [
-              'title' => 'Some Tech title 3', 
-              'content' => 'Some Tech content 3', 
-              'date' => 'May 20, 2009', 
-              'author' => 'Alex',
-            ],
-            [
-              'title' => 'Some Tech title 4', 
-              'content' => 'Some Tech content 4', 
-              'date' => 'Sept 13, 2018', 
-              'author' => 'Tommi sularaha',
-            ],
-        ];
-        view('tech', compact('title', 'posts'));
+        view('tech', compact('title', 'articles'));
     }
     
     public function forms() {
